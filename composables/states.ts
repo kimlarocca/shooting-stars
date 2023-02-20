5f11d34050b432bda7b22c3ea6de325881d19ade
Thomas Bono <thomasbono.com@gmail.com>
2023-02-20 18:58:40 -0500

    started dynamic menu

---
+++ composables/states.ts
@@ -0,0 +1,5 @@
+//export const useGlobalMaxContentWidth = () => useState<boolean>('globalMaxContentWidth', () => 672)
+
+//export const useCurrentBreakpoint = () => useState<boolean>('currentBreakpoint', () => null)
+
+export const useMenuData = () => useState<object>('globalMenuData', () => { return {} })
