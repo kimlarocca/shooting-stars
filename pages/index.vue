375e6207eef1d65f31283555fdc27ff6d72e51a3
Thomas Bono <thomasbono.com@gmail.com>
2023-02-22 01:10:00 -0500

    started person component and container query

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,44 @@ const story = await useAsyncStoryblok('home', { version: 'draft' })
 </script>
 
 <template>
+  <pre>{{ story }}</pre>
   <StoryblokComponent v-if="story" :blok="story.content" />
+  <div class="grid mt-5">
+    <div class="box col-6">
+      <div class="container">
+        <div class="root flex">
+          <div class="box">1</div>
+          <div class="box">2</div>
+          <div class="box">3</div>
+        </div>
+      </div>
+    </div>
+    <div class="box col-3">2</div>
+    <div class="box col-3">3</div>
+  </div>
 </template>
+
+<style lang="scss">
+.grid {
+  justify-content: space-around;
+  .box {
+    background-color: tan;
+  }
+  .container {
+    container-type: inline-size;
+  }
+  .root {
+    gap: 1rem;
+    justify-content: space-between;
+    .box {
+      background-color: lightblue;
+    }
+  }
+}
+@container (max-width: 400px) {
+  .root {
+    gap: 3rem;
+    font-size: 2.25rem;
+  }
+}
+</style>
---
+++ storyblok/Person.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+<template>
+  <div v-editable="blok">
+    <div class="person-blok">
+      <div class="container">
+        <h1>{{ blok.name }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.person-blok {
+  .container {
+    container-type: inline-size;
+    background-color: lightblue;
+  }
+}
+@container (max-width: 1200px) {
+  .person-blok {
+    h1 {
+      font-size: 1.25rem;
+    }
+  }
+}
+</style>
